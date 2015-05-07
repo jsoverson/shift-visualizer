b2e2fd37dedef379da37fb101c095e5cf7926cec
Michael Ficarra <github@michael.ficarra.me>
2015-05-07 13:58:42 -0700

    disable location tracking

--- public/js/main.js
+++ public/js/main.js
@@ -80,7 +80,7 @@ session.setMode("ace/mode/javascript");
 function onChange() {
   var code = editor.getValue();
   try {
-    var ast = shift.parseModule(code, { loc: true, earlyErrors : true });
+    var ast = shift.parseModule(code, { loc: false, earlyErrors : true });
     displayTree(ast);
   } catch (ex) {
     displayError(ex);
